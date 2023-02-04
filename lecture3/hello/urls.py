255705e889f00f38f347c18ed10c0cbff906d08f
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-04 04:31:39 +0000

    Sat, Feb 4, 2023, 11:31 AM +07:00

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("brian", views.brian, name="brian")
+    path("brian", views.brian, name="brian"),
+    path("<str:name>", views.greet, name="greet")
 ]
