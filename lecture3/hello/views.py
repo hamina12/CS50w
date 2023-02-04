69cbc350847d6f8807e295e04a4dddcf0fcf4180
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-04 04:43:44 +0000

    Sat, Feb 4, 2023, 11:43 AM +07:00

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -6,6 +6,6 @@ def index(request):
     return render(request, "hello/index.html")
 
 def greet(request, name):
-    return render(request, "hello/greet.html"), {
+    return render(request, "hello/greet.html", {
         "name": name.capitalize()
-    }
+    })
