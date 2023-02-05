d6b05d529ea522eb297c26df1e214043a5c38c37
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-05 06:58:52 +0000

    Sun, Feb 5, 2023, 1:58 PM +07:00

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -11,7 +11,7 @@ class NewTaskForm(forms.Form):
 # Create your views here.
 def index(request):
     if "tasks" not in request.session:
-        request.session["task"] = []
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
         "tasks": request.session["tasks"]
     })
@@ -20,7 +20,7 @@ def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
-            task = form.cleaned_data["task"]
+            task = form.cleaned_data["tasks"]
             request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
