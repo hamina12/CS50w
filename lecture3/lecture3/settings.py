596103b23ccadad29f38bd8acbc84f1ddd56b98c
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-05 05:59:03 +0000

    Sun, Feb 5, 2023, 12:59 PM +07:00

--- lecture3/lecture3/settings.py
+++ lecture3/lecture3/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hello',
     'newyear',
-    'task',
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
