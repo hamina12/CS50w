cf5b1ac3117bab0e6d9a8a45b6fb7828bcd64760
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-06 02:23:23 +0000

    Mon, Feb 6, 2023, 9:23 AM +07:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -15,6 +15,6 @@ def save(request):
 
 def load(request):
     return render(request, "encyclopedia/load.html", {
-        util.get_entry(util.list_entries(random()))
+        "random" = util.get_entry(util.list_entries("CSS"))
     })
 
