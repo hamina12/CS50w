5c4e1c3e9ffc20eb4e8fff28d3e8d779875e7347
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-09 05:01:00 +0000

    Thu, Feb 9, 2023, 12:01 PM +07:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -33,24 +33,4 @@ def entry(request, title):
             "title" : name
         })
 
-def randompage(request):
-    if request.method == "POST":
-        q = request.POST.get('q')
-            try:
-                name = markdown2.markdown(util.get_entry(q))
-                return render(request, "encyclopedia/entry.html", {
-                "title" : name
-                })
-            except:
-                name = "Requested page was not found."
-                return render(request, "encyclopedia/entry.html", {
-                    "title" : name
-                })
-
-    entri = util.list_entries()
-    n = random.randrange(len(entri))
-    name = markdown2.markdown(util.get_entry(entri[n]))
-    return render(request, "encyclopedia/entry.html", {
-        "title" : name
-    })
 
