c4cfadfa8f1faf2ec8ae38aba8c69c330171b811
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-09 07:23:43 +0000

    Thu, Feb 9, 2023, 2:23 PM +07:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,10 +7,20 @@ from . import util
 def index(request):
     if request.method == "POST":
         q = request.POST.get('q')
-        name = markdown2.markdown(util.get_entry(q))
-        return render(request, "encyclopedia/entry.html", {
-        "title" : name
-        })
+        try:
+            name = markdown2.markdown(util.get_entry(q))
+            return render(request, "encyclopedia/entry.html", {
+                "title" : name
+            })
+        except:
+            ent = []
+            entries = util.list_entries()
+            for entri in entries:
+                if q in entri:
+                    ent.append(entri)
+            return render(request, "encyclopedia/search.html", {
+                "entries" : ent
+            })
 
 
     return render(request, "encyclopedia/index.html", {
