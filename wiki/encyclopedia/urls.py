ad2f682edba4d38e2a901de5a0dc4f48b0f8bec8
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-09 13:36:46 +0000

    Thu, Feb 9, 2023, 8:36 PM +07:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create", views.create, name="create"),
-    path("<str:title>", views.entry, name="entry"),
-    path("edit/<str:title>", views.edit, name="edit"),
-    path("random", views.randompage, name="randompage")
+    path("<str:title>/", views.entry, name="entry"),
+    path("search/", views.get_entry_or_search_results, name="search"),
+    path("create/", views.create, name="create"),
+    path("edit/<str:title>/", views.edit, name="edit"),
+    path("random/", views.randompage, name="randompage"),
 ]
