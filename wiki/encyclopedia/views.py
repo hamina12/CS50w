dc43c5ef2c4dff4a66571138d2f4a39d5b710e6c
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-09 10:33:41 +0000

    Thu, Feb 9, 2023, 5:33 PM +07:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -68,7 +68,7 @@ def create(request):
 
     return render(request, "encyclopedia/create.html")
 
-def edit(request, title):
+def edit(request):
     if request.method == "POST":
         util.save_entry(title, request.POST.get('text'))
         name = markdown2.markdown(util.get_entry(request.POST.get('title')))
