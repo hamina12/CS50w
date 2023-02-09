6244b93f2894f68cee188623555939d7e710b33e
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-09 04:45:01 +0000

    Thu, Feb 9, 2023, 11:45 AM +07:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,7 +5,18 @@ from . import util
 
 
 def index(request):
-    if 
+    if request.method == "POST":
+        q = request.get.form('q')
+        try:
+            name = markdown2.markdown(util.get_entry(q))
+            return render(request, "encyclopedia/entry.html", {
+            "title" : name
+            })
+        except:
+            name = "Requested page was not found."
+            return render(request, "encyclopedia/entry.html", {
+                "title" : name
+            })
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
