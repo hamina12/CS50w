6c1ee372b6ac089a232d3cce954f0e2e8ca2fe7c
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-09 04:13:56 +0000

    Thu, Feb 9, 2023, 11:13 AM +07:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
-
+import markdown2
+import random
 from . import util
 
 
 def index(request):
     if request.method == "POST":
-        
+        if 'q' in request.POST:
+            q = request.POST.get('q')
+            try:
+                title = markdown2.markdown(util.get_entry(q))
+                return render(request, "encyclopedia/entry.html", {
+                    "title" : title
+                })
+            except:
+                title = "Requested page was not found."
+                return render(request, "encyclopedia/entry.html", {
+                    "title" : title
+                })
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, title):
-    return render(request, "encyclopedia/entry.html", {
-        "title" : util.get_entry(title)
-    })
+    try:
+        name = markdown2.markdown(util.get_entry(title))
+        return render(request, "encyclopedia/entry.html", {
+        "title" : name
+        })
+    except:
+        name = "Requested page was not found."
+        return render(request, "encyclopedia/entry.html", {
+            "title" : name
+        })
+
+
