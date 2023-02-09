e3989a985e7f1354ddb17d8ab79d9e2f3955f099
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-09 13:30:57 +0000

    Thu, Feb 9, 2023, 8:30 PM +07:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("create", views.create, name="create"),
     path("<str:title>", views.entry, name="entry"),
     path("edit/<str:title>", views.edit, name="edit"),
-    path("<str:title>", views.randompage, name="randompage")
+    path("random", views.randompage, name="randompage")
 ]
