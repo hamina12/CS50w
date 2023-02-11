c31c681b4577c7010b76a3241a43fbec952db52a
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-11 09:08:19 +0000

    Sat, Feb 11, 2023, 4:08 PM +07:00

--- lab4/airline/flights/admin.py
+++ lab4/airline/flights/admin.py
@@ -4,7 +4,7 @@ from .models import Flight, Airport, Passenger
 
 # Register your models here.
 class FlightAdmin(admin.ModelAdmin):
-    list_display= ("id", "origin", "destination", "duration")
+    list_display = ("id", "origin", "destination", "duration")
 
 class PassengerAdmin(admin.ModelAdmin):
     filter_horizontal = ("flights",)
