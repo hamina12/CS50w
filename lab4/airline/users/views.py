cb492bb875932eea396e1908e2eac4203a7d8cb7
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-11 09:18:53 +0000

    Sat, Feb 11, 2023, 4:18 PM +07:00

--- lab4/airline/users/views.py
+++ lab4/airline/users/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+def login_view(request):
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    pass
