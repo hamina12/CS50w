31d9216046a6518a56444bd18a063b35de4b829c
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-11 09:42:22 +0000

    Sat, Feb 11, 2023, 4:42 PM +07:00

--- lab4/airline/users/views.py
+++ lab4/airline/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -8,6 +9,17 @@ def index(request):
         return HttpResponseRedirect(reverse("login"))
 
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message" : "Invalid"
+            })
     return render(request, "users/login.html")
 
 def logout_view(request):
