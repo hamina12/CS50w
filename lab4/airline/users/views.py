d98b6c7afa202db5fbc3f26b594f477502f7ce2c
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-11 09:23:09 +0000

    Sat, Feb 11, 2023, 4:23 PM +07:00

--- lab4/airline/users/views.py
+++ lab4/airline/users/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
