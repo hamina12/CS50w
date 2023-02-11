23016f4a48444f409f2ff0173e58b44be7c89eb3
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-11 07:47:52 +0000

    Sat, Feb 11, 2023, 2:47 PM +07:00

--- lab4/airline/flights/models.py
+++ lab4/airline/flights/models.py
@@ -7,4 +7,4 @@ class Flight(models.Model):
     duration = models.IntegerField()
 
     def __str__(self):
-        return f"{self.id}: {self.origin} to {self.destination}
+        return f"{self.id}: {self.origin} to {self.destination}"
