1a31fa4c14a45a771090c5db701651bc1a6cd939
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-12 06:30:51 +0000

    Sun, Feb 12, 2023, 1:30 PM +07:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
     parent = models.ForeignKey('self',blank=True, null=True ,related_name='children')
 
     class Meta:
-        unique_together = ('ETC.', 'TECH', 'HOME', )
+        unique_together = ('ETC.', 'TECH', 'HOME', 'GENERAL')
         verbose_name_plural = "categories"
 
 
