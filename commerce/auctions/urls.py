5640e77c2c3c679d14d78bb8da9f2b53ddc71f7d
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-12 08:25:15 +0000

    Sun, Feb 12, 2023, 3:25 PM +07:00

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.upload_image, name="upload"),
-    path("", views.display_image, name="display"),
+    path("display", views.display_image, name="display"),
     #path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
