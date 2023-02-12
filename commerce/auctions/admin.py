f425820758f0b114f75b8aaa7aaf98318a6a406e
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-12 05:49:59 +0000

    Sun, Feb 12, 2023, 12:49 PM +07:00

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, Aunction
+from .models import User, Aunction, Bid, Comment
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Aunction)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
 
 class Aunction(models.Model):
     name = models.CharField(max_length=64)
-    price = models.DecimalField(max_digits=, decimal_places=2)
+    price = models.DecimalField(max_digits=64, decimal_places=2)
     pic = models.ImageField()
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Aunction(models.Model):
 class Bid(models.Model):
     aunction = models.ForeignKey(Aunction, on_delete=models.CASCADE, related_name="item")
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="lastbid")
-    current = models.DecimalField(max_digits=None, decimal_places=2)
+    current = models.DecimalField(max_digits=64, decimal_places=2)
 
     def __str__(self):
         return f"{self.item}: {self.user}|{self.current}"
