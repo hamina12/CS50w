2879a67147d8e68312362e1a9380a3cc394fcff2
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-12 07:52:16 +0000

    Sun, Feb 12, 2023, 2:52 PM +07:00

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Application definition
 
 INSTALLED_APPS = [
