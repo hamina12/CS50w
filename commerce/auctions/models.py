571d3af13663e6982aae1869901717fa4dbd1c7f
Wiwatpon Lertworasirikul <hamlert33@gmail.com>
2023-02-12 06:25:45 +0000

    Sun, Feb 12, 2023, 1:25 PM +07:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -2,11 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
 class User(AbstractUser):
     pass
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
+
+Category = [ 'ETC.', 'TECH', 'HOME']
 
 class Aunction(models.Model):
     name = models.CharField(max_length=64)
